18c716e6581b184bab8b46fbaa0b77fc6514445b
Philip Dutra <monsterphilip10@gmail.com>
2023-03-15 21:55:55 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10);
+let num3 = 10;
+let num4 = 6;
+let mod = num3%num4;
+let num5 = 20;
+let max = Math.max(num2, num3, num4, num5);
